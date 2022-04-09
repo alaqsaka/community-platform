e04f2ff142db650fcd8c6ee2f38be4a6f38efd9e
alaqsaka <aqsaabidin15@gmail.com>
2022-04-09 17:19:31 +0700

    installing dependecies, create user model, create user controller, create user route, auth api

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 import reviewRoutes from "./routes/reviews.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -26,6 +27,7 @@ app.use(
 app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/reviews", reviewRoutes);
+app.use("user", userRoutes);
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
