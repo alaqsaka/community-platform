7d36fb7140915c6a13c26440a942410ba8a30d2e
alaqsaka <aqsaabidin15@gmail.com>
2022-04-11 14:26:54 +0700

    feat: refactoring app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import {
   ThemeProvider,
   createTheme,
@@ -10,6 +10,7 @@ import "./index.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
   const theme = createTheme({
@@ -24,16 +25,25 @@ const App = () => {
     },
   });
 
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
 
       <BrowserRouter>
         <Navbar />
-        <Container maxWidth="lg">
+        <Container maxWidth="xl">
           <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path="/auth" exact element={<Auth />} />
+            <Route path="/" exact element={<Navigate to="/posts" />} />
+            <Route path="/posts" exact element={<Home />} />
+            <Route path="/posts/search" exact element={<Home />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+            <Route
+              path="/auth"
+              exact
+              element={!user ? <Auth /> : <Navigate to="/posts" />}
+            />
           </Routes>
         </Container>
       </BrowserRouter>
